8f87daf7301711df9973ef0cc87537e5e119c295
hfilipe- <hfilipe-@student.42porto.com>
2025-06-26 16:14:45 +0100

    first ex

---
+++ ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfilipe- <hfilipe-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 14:58:24 by hfilipe-          #+#    #+#             */
+/*   Updated: 2025/06/26 16:06:06 by hfilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BITCOINEXCHANGE_CPP
+# define BITCOINEXCHANGE_CPP
+
+# include <iostream>
+# include <fstream>
+# include <list>
+# include <sstream>
+# include <string>
+# include <stdlib.h>
+
+# endif
